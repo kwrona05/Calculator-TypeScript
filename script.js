7d18528c57f112d6237c0ee0cc3108317e0485f7
kwrona05 <kwrona05@gmail.com>
2024-06-07 08:14:17 +0200

    comment script.js

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-const number1 = document.querySelector('.firstNumber');
-const number2 = document.querySelector('.secondNumber');
+// const number1 = document.querySelector('.firstNumber');
+// const number2 = document.querySelector('.secondNumber');
 
-const plusBtn = document.querySelector('.plus');
-const minusBtn = document.querySelector('.minus');
-const multiplyBtn = document.querySelector('.multiply');
-const divideBtn = document.querySelector('.divide');
+// const plusBtn = document.querySelector('.plus');
+// const minusBtn = document.querySelector('.minus');
+// const multiplyBtn = document.querySelector('.multiply');
+// const divideBtn = document.querySelector('.divide');
 
-const paragraph = document.querySelector('.result')
+// const paragraph = document.querySelector('.result')
 
-function plus() {
-    const result = parseFloat(number1.value) + parseFloat(number2.value)
-    paragraph.textContent = result
-}
+// function plus() {
+//     const result = parseFloat(number1.value) + parseFloat(number2.value)
+//     paragraph.textContent = result
+// }
 
-function minus() {
-    const result = parseFloat(number1.value) - parseFloat(number2.value)
-    paragraph.textContent = result
-}
+// function minus() {
+//     const result = parseFloat(number1.value) - parseFloat(number2.value)
+//     paragraph.textContent = result
+// }
 
-function multiply() {
-    const result = parseFloat(number1.value) * parseFloat(number2.value)
-    paragraph.textContent = result
-}
+// function multiply() {
+//     const result = parseFloat(number1.value) * parseFloat(number2.value)
+//     paragraph.textContent = result
+// }
 
-function divide() {
-    const result = parseFloat(number1.value) / parseFloat(number2.value)
-    paragraph.textContent = result
-}
+// function divide() {
+//     const result = parseFloat(number1.value) / parseFloat(number2.value)
+//     paragraph.textContent = result
+// }
 
 
-plusBtn.addEventListener('click', plus)
-minusBtn.addEventListener('click', minus)
-multiplyBtn.addEventListener('click', multiply)
-divideBtn.addEventListener('click', divide)
+// plusBtn.addEventListener('click', plus)
+// minusBtn.addEventListener('click', minus)
+// multiplyBtn.addEventListener('click', multiply)
+// divideBtn.addEventListener('click', divide)
